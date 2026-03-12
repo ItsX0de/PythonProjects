e6663a532444a45439874aaf6379750841b7e37a
X0de <seanremington1221@gmail.com>
2026-03-12 22:23:38 +0200

    Add files via upload

---
+++ comparison_examples.py
@@ -0,0 +1,37 @@
+# comparison_examples.py
+
+# Equal to
+print(5 == 5)
+
+# Not equal to
+print(5 != 3)
+
+# Greater than
+print(10 > 4)
+
+# Less than
+print(2 < 7)
+
+# Greater than or equal to
+print(5 >= 5)
+
+# Less than or equal to
+print(3 <= 8)
+
+
+# Using comparison operators with variables
+
+a = 10
+b = 5
+
+print(a > b)
+print(a == b)
+print(a != b)
+
+# Using comparisons with input
+
+num = int(input("Enter a number: "))
+if num > 10:
+    print("The number is greater than 10")
+else:
+    print("The number is 10 or less")
