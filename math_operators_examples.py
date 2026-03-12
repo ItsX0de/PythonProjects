2b4da696b1c3c3618985e692ca6582bc47b7866a
X0de <seanremington1221@gmail.com>
2026-03-12 17:34:50 +0200

    Add files via upload

---
+++ math_operators_examples.py
@@ -0,0 +1,45 @@
+# math_operators_examples.py
+
+# Addition
+print(5 + 3)
+
+# Subtraction
+print(10 - 4)
+
+# Multiplication
+print(6 * 2)
+
+# Division
+print(10 / 2)
+
+
+# Using variables
+
+a = 8
+b = 4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+
+# Power (exponents)
+
+print(2 ** 3)  # 2^3
+
+
+# Modulus (remainder)
+
+print(10 % 3)  # remainder after division
+
+
+# Mini example
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter anouther number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
