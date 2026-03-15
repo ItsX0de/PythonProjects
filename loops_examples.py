ea19e50e914864a2c22dd05db6483c9747f9a88b
X0de <seanremington1221@gmail.com>
2026-03-15 22:01:52 +0200

    Add files via upload

---
+++ loops_examples.py
@@ -0,0 +1,32 @@
+
+# Loops_examples.py
+
+# FOR LOOP
+# Runs a certain number of times
+
+for i in range(5):
+    print("Hello")
+
+
+# FOR LOOPS with numbers
+
+    for i in range(1, 6):
+        print(i)
+
+
+# WHILE LOOP
+# Runs while a condition is true
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+
+# Using a loop with input
+
+number =  int(input("Enter a number: "))
+
+for i in range(1, number + 1):
+    print (i)
