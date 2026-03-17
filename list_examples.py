7a3091150b1c533f932364c53b8396d3982f4bd6
X0de <seanremington1221@gmail.com>
2026-03-17 13:51:25 +0200

    Add files via upload

---
+++ list_examples.py
@@ -0,0 +1,19 @@
+# Creating a list
+numbers = [10, 20, 30]
+print(numbers) 
+
+# Accessing elements
+print(numbers[0])
+print(numbers[1])
+
+# Adding an element
+numbers.append(40)
+print(numbers)
+
+# Removing an element
+numbers.remove(20)
+print(numbers)
+
+# Looping through a list
+for num in numbers:
+    print(num)
